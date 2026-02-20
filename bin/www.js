68acab6c9d3d311aa19e9c76c8d36da54da81222
viyansyah <viyansyah@gmail.com>
2026-02-20 07:17:41 +0700

    fix:load dotenv

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 const app = require("../app")
-const port = 3400
+const port = process.env.PORT || 3400
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
